8402ac61d85952e9bc63b13783f1a556a22d3dac
Johnny Ho <johnny5550822@gmail.com>
2020-04-05 12:54:24 -0700

    add cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An information extraction and visualization tool for covd-19',
+    author='johnny_and_panayiotis',
+    license='MIT',
+)
