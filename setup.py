0190be788d6b4bcc42132e6d0d5783879cf79454
Johnny Ho <johnny5550822@gmail.com>
2020-04-05 13:09:57 -0700

    update setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='covid',
     packages=find_packages(),
     version='0.1.0',
     description='An information extraction and visualization tool for covd-19',
