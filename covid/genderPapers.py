911c2822ee106c1e3b11d7b008bc2a9ccca4ff27
pp89@ucla.edu <pp89@ucla.edu>
2020-04-05 17:41:13 -0700

    tested all combinations of patterns and fized typo

--- covid/genderPapers.py
+++ covid/genderPapers.py
@@ -58,57 +58,78 @@ def regexQueryDf(df,cols,patterns,operatorPattern='AND',operatorColumn='OR'):
     elif operatorPattern=='AND' and operatorColumn=='OR':
         cond = pd.Series([True]*len(df))
         for pattern in patterns: 
-            cond = cond & (df.progress_apply(lambda row: all(row[cols].str.contains(pattern)),axis=1))
+            cond = cond & (df.progress_apply(lambda row: any(row[cols].str.contains(pattern)),axis=1))
         return cond
     elif operatorPattern=='OR' and operatorColumn=='OR':
         cond = pd.Series([False]*len(df))
         for pattern in patterns: 
-            cond = cond | (df.progress_apply(lambda row: all(row[cols].str.contains(pattern)),axis=1))
+            cond = cond | (df.progress_apply(lambda row: any(row[cols].str.contains(pattern)),axis=1))
         return cond
 
 ############################################################################################################
 
+if __name__ == "__main__":
 
-####### gender related words
+    ####### gender related words
 
-gender_words = ['male','female','sex','gender']
-pattern_gender = ' | '.join(getSynonymsAntonymnsListOfWords(gender_words))
-pattern_gender = pattern_gender.replace(' |','|(?i)') #regex
-
-
-############################### corona virus string match
-# keywords from here https://coviz.apps.allenai.org/bc5cdr/?d=bc5cdr&l=40&ftm=11444
+    gender_words = ['male','female','sex','gender']
+    pattern_gender = ' | '.join(getSynonymsAntonymnsListOfWords(gender_words))
+    pattern_gender = pattern_gender.replace(' |','|(?i)') #regex
 
 
-pattern_COVID = 'respiratory tract infection |virus infection |respiratory syncytial virus | \
-                 lipopolysaccharide |death |acute respiratory distress syndrome |acute respiratory failure | \
-                 H1N1 viral infection |rubella virus infection |influenza virus infection |human immunodeficiency virus | \
-                 irritation of the respiratory tract |Zika Virus Infection |Ebola and Zika virus infection | \
-                 porcine reproductive and respiratory syndrome |TAP |influenza virus A |Thrombocytopenia Syndrome Virus Infection | \
-                 SARS-CoV-2 |respiratory syncitial virus |skin or mucous membrane lesions |upper respiratory infection | \
-                 H5N1 viral infection |herpes simplex virus type 1 |human immunodeficiency virus type 1 |gastrointestinal viral infection | \
-                 reproductive and respiratory syndrome virus infection |porcine reproductive and respiratory syndrome virus | \
-                 hepatitis A virus |acquired immunodeficiency syndrome |parainfluenza virus 3 | \
-                 nosocomial viral respiratory infections |coronavirus OC43 infection |IFN |H3N2 virus infection | \
-                 dsRNA |dsDNA |long QT syndrome |liver cell necrosis |latent TB infection |Pulmonary Coronavirus Infection | \
-                 Dengue virus Type |neurotropic coronavirus virus |Leukocyte adhesion deficiency II syndrome | \
-                 Human T-cell leukemia virus type 1 |Human T-cell leukemia virus type | \
-                 infection of the central nervous system |infection of the pulmonary parenchyma'
-
-pattern_COVID = pattern_COVID.replace(' |','|(?i)')
+    ############################### corona virus string match
+    # keywords from here https://coviz.apps.allenai.org/bc5cdr/?d=bc5cdr&l=40&ftm=11444
 
 
-######## joining paterns
-file = 'metadata_pre_proc.csv'
+    pattern_COVID = 'respiratory tract infection |virus infection |respiratory syncytial virus | \
+                    lipopolysaccharide |death |acute respiratory distress syndrome |acute respiratory failure | \
+                    H1N1 viral infection |rubella virus infection |influenza virus infection |human immunodeficiency virus | \
+                    irritation of the respiratory tract |Zika Virus Infection |Ebola and Zika virus infection | \
+                    porcine reproductive and respiratory syndrome |TAP |influenza virus A |Thrombocytopenia Syndrome Virus Infection | \
+                    SARS-CoV-2 |respiratory syncitial virus |skin or mucous membrane lesions |upper respiratory infection | \
+                    H5N1 viral infection |herpes simplex virus type 1 |human immunodeficiency virus type 1 |gastrointestinal viral infection | \
+                    reproductive and respiratory syndrome virus infection |porcine reproductive and respiratory syndrome virus | \
+                    hepatitis A virus |acquired immunodeficiency syndrome |parainfluenza virus 3 | \
+                    nosocomial viral respiratory infections |coronavirus OC43 infection |IFN |H3N2 virus infection | \
+                    dsRNA |dsDNA |long QT syndrome |liver cell necrosis |latent TB infection |Pulmonary Coronavirus Infection | \
+                    Dengue virus Type |neurotropic coronavirus virus |Leukocyte adhesion deficiency II syndrome | \
+                    Human T-cell leukemia virus type 1 |Human T-cell leukemia virus type | \
+                    infection of the central nervous system |infection of the pulmonary parenchyma'
 
-df = pd.read_csv('../Data/'+file)
+    pattern_COVID = pattern_COVID.replace(' |','|(?i)')
 
 
-cond_COVID_gender = regexQueryDf(df,['title'],[pattern_COVID,pattern_gender],operator='AND')
+    ######## joining paterns
+    file = 'metadata_pre_proc.csv'
 
-print('Number of papers found: ', np.sum(cond_gender))
+    df = pd.read_csv('../Data/'+file,nrows=500) #sample of 500 papers
 
-df = df[cond_COVID_gender].reset_index(drop=True)
+    ######## all combinations of all patterns to be satisfied in all columns
+    cols = ['title','abstract']
 
-df.to_csv('../Data/gender_papers.csv')
+    cond_COVID_gender = regexQueryDf(df,cols,[pattern_COVID,pattern_gender],operatorPattern='AND',
+                                                                                operatorColumn='OR')
+
+    print('Number of papers found: ', np.sum(cond_COVID_gender))
+
+    cond_COVID_gender = regexQueryDf(df,cols,[pattern_COVID,pattern_gender],operatorPattern='AND',
+                                                                                operatorColumn='AND')
+
+    print('Number of papers found: ', np.sum(cond_COVID_gender))
+
+    cond_COVID_gender = regexQueryDf(df,cols,[pattern_COVID,pattern_gender],operatorPattern='OR',
+                                                                                operatorColumn='AND')
+
+    print('Number of papers found: ', np.sum(cond_COVID_gender))
+
+    cond_COVID_gender = regexQueryDf(df,cols,[pattern_COVID,pattern_gender],operatorPattern='OR',
+                                                                                operatorColumn='OR')
+
+    print('Number of papers found: ', np.sum(cond_COVID_gender))
+
+
+
+    df = df[cond_COVID_gender].reset_index(drop=True)
+
+    df.to_csv('../Data/gender_papers.csv')
 
