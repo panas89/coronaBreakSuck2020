155f190f61d1d01399a86c4daec1566d7d5f43a6
pp89@ucla.edu <pp89@ucla.edu>
2020-04-07 01:06:52 -0700

    fixed 2 bugs using any() and all(), replacing NaNs with False fixed bug

--- covid/genderPapers.py
+++ covid/genderPapers.py
@@ -47,23 +47,28 @@ def regexQueryDf(df,cols,patterns,operatorPattern='AND',operatorColumn='OR'):
 
     if operatorPattern=='AND' and operatorColumn=='AND':
         cond = pd.Series([True]*len(df))
-        for pattern in patterns: 
-            cond = cond & (df.progress_apply(lambda row: all(row[cols].str.contains(pattern)),axis=1))
+        # print(cond)
+        for pattern in patterns:
+            # print(df.progress_apply(lambda row: row[cols].str.contains(pattern).fillna(False).all(),axis=1))
+            # print(df.progress_apply(lambda row: row[cols].str.contains(pattern).fillna(False),axis=1))
+            cond = cond & (df.progress_apply(lambda row: row[cols].str.contains(pattern).fillna(False).all(),axis=1))
+        #     print(cond)
+        # print(cond)
         return cond
     elif operatorPattern=='OR' and operatorColumn=='AND':
         cond = pd.Series([False]*len(df))
         for pattern in patterns: 
-            cond = cond | (df.progress_apply(lambda row: all(row[cols].str.contains(pattern)),axis=1))
+            cond = cond | (df.progress_apply(lambda row: row[cols].str.contains(pattern).fillna(False).all(),axis=1))
         return cond
     elif operatorPattern=='AND' and operatorColumn=='OR':
         cond = pd.Series([True]*len(df))
         for pattern in patterns: 
-            cond = cond & (df.progress_apply(lambda row: any(row[cols].str.contains(pattern)),axis=1))
+            cond = cond & (df.progress_apply(lambda row: row[cols].str.contains(pattern).fillna(False).any(),axis=1))
         return cond
     elif operatorPattern=='OR' and operatorColumn=='OR':
         cond = pd.Series([False]*len(df))
         for pattern in patterns: 
-            cond = cond | (df.progress_apply(lambda row: any(row[cols].str.contains(pattern)),axis=1))
+            cond = cond | (df.progress_apply(lambda row: row[cols].str.contains(pattern).fillna(False).any(),axis=1))
         return cond
 
 ############################################################################################################
@@ -72,29 +77,35 @@ if __name__ == "__main__":
 
     ####### gender related words
 
-    gender_words = ['male','female','sex','gender']
-    pattern_gender = ' | '.join(getSynonymsAntonymnsListOfWords(gender_words))
-    pattern_gender = pattern_gender.replace(' |','|(?i)') #regex
+    # gender_words = ['male','female','sex','gender']
+    # pattern_gender = ' | '.join(getSynonymsAntonymnsListOfWords(gender_words))
+    # pattern_gender = pattern_gender.replace(' |','|(?i)') #regex
+
+    #simple query
+    pattern_gender = 'male | female | sex | gender'
 
 
     ############################### corona virus string match
     # keywords from here https://coviz.apps.allenai.org/bc5cdr/?d=bc5cdr&l=40&ftm=11444
 
 
-    pattern_COVID = 'respiratory tract infection |virus infection |respiratory syncytial virus | \
-                    lipopolysaccharide |death |acute respiratory distress syndrome |acute respiratory failure | \
-                    H1N1 viral infection |rubella virus infection |influenza virus infection |human immunodeficiency virus | \
-                    irritation of the respiratory tract |Zika Virus Infection |Ebola and Zika virus infection | \
-                    porcine reproductive and respiratory syndrome |TAP |influenza virus A |Thrombocytopenia Syndrome Virus Infection | \
-                    SARS-CoV-2 |respiratory syncitial virus |skin or mucous membrane lesions |upper respiratory infection | \
-                    H5N1 viral infection |herpes simplex virus type 1 |human immunodeficiency virus type 1 |gastrointestinal viral infection | \
-                    reproductive and respiratory syndrome virus infection |porcine reproductive and respiratory syndrome virus | \
-                    hepatitis A virus |acquired immunodeficiency syndrome |parainfluenza virus 3 | \
-                    nosocomial viral respiratory infections |coronavirus OC43 infection |IFN |H3N2 virus infection | \
-                    dsRNA |dsDNA |long QT syndrome |liver cell necrosis |latent TB infection |Pulmonary Coronavirus Infection | \
-                    Dengue virus Type |neurotropic coronavirus virus |Leukocyte adhesion deficiency II syndrome | \
-                    Human T-cell leukemia virus type 1 |Human T-cell leukemia virus type | \
-                    infection of the central nervous system |infection of the pulmonary parenchyma'
+    # pattern_COVID = 'respiratory tract infection |virus infection |respiratory syncytial virus | \
+    #                 lipopolysaccharide |death |acute respiratory distress syndrome |acute respiratory failure | \
+    #                 H1N1 viral infection |rubella virus infection |influenza virus infection |human immunodeficiency virus | \
+    #                 irritation of the respiratory tract |Zika Virus Infection |Ebola and Zika virus infection | \
+    #                 porcine reproductive and respiratory syndrome |TAP |influenza virus A |Thrombocytopenia Syndrome Virus Infection | \
+    #                 SARS-CoV-2 |respiratory syncitial virus |skin or mucous membrane lesions |upper respiratory infection | \
+    #                 H5N1 viral infection |herpes simplex virus type 1 |human immunodeficiency virus type 1 |gastrointestinal viral infection | \
+    #                 reproductive and respiratory syndrome virus infection |porcine reproductive and respiratory syndrome virus | \
+    #                 hepatitis A virus |acquired immunodeficiency syndrome |parainfluenza virus 3 | \
+    #                 nosocomial viral respiratory infections |coronavirus OC43 infection |IFN |H3N2 virus infection | \
+    #                 dsRNA |dsDNA |long QT syndrome |liver cell necrosis |latent TB infection |Pulmonary Coronavirus Infection | \
+    #                 Dengue virus Type |neurotropic coronavirus virus |Leukocyte adhesion deficiency II syndrome | \
+    #                 Human T-cell leukemia virus type 1 |Human T-cell leukemia virus type | \
+    #                 infection of the central nervous system |infection of the pulmonary parenchyma'
+
+    #simple query to test
+    pattern_COVID = 'covid | SARS-CoV-2'
 
     pattern_COVID = pattern_COVID.replace(' |','|(?i)')
 
@@ -102,30 +113,32 @@ if __name__ == "__main__":
     ######## joining paterns
     file = 'metadata_pre_proc.csv'
 
-    df = pd.read_csv('../Data/'+file,nrows=500) #sample of 500 papers
+    df = pd.read_csv('../Data/'+file,nrows=20000) #sample of 20000 papers
+
 
     ######## all combinations of all patterns to be satisfied in all columns
     cols = ['title','abstract']
 
     cond_COVID_gender = regexQueryDf(df,cols,[pattern_COVID,pattern_gender],operatorPattern='AND',
-                                                                                operatorColumn='OR')
-
-    print('Number of papers found: ', np.sum(cond_COVID_gender))
-
-    cond_COVID_gender = regexQueryDf(df,cols,[pattern_COVID,pattern_gender],operatorPattern='AND',
                                                                                 operatorColumn='AND')
 
     print('Number of papers found: ', np.sum(cond_COVID_gender))
 
-    cond_COVID_gender = regexQueryDf(df,cols,[pattern_COVID,pattern_gender],operatorPattern='OR',
-                                                                                operatorColumn='AND')
+    # cond_COVID_gender = regexQueryDf(df,cols,[pattern_COVID,pattern_gender],operatorPattern='OR',
+    #                                                                             operatorColumn='AND')
 
-    print('Number of papers found: ', np.sum(cond_COVID_gender))
+    # print('Number of papers found: ', np.sum(cond_COVID_gender))
 
-    cond_COVID_gender = regexQueryDf(df,cols,[pattern_COVID,pattern_gender],operatorPattern='OR',
-                                                                                operatorColumn='OR')
+    # cond_COVID_gender = regexQueryDf(df,cols,[pattern_COVID,pattern_gender],operatorPattern='AND',
+    #                                                                             operatorColumn='OR')
 
-    print('Number of papers found: ', np.sum(cond_COVID_gender))
+    # print('Number of papers found: ', np.sum(cond_COVID_gender))
+
+
+    # cond_COVID_gender = regexQueryDf(df,cols,[pattern_COVID,pattern_gender],operatorPattern='OR',
+    #                                                                             operatorColumn='OR')
+
+    # print('Number of papers found: ', np.sum(cond_COVID_gender))
 
 
 
