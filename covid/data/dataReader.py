cfb0cac5f80b259520c250e1f6e992c0e5e71532
Panayiotis Petousis <pp89@ucla.edu>
2020-05-08 12:19:05 -0700

    Pulling raw data (#15)
    * added curl and unzip commands to get all sources o data csv and json
    
    * added save requirements make command, testing make data
    
    * updated preprocessor to handle json and csv
    
    * converted all files json files to csv
    
    * merged datasets with metadata

--- covid/data/dataReader.py
+++ covid/data/dataReader.py
@@ -68,7 +68,22 @@ def format_affiliation(affiliation):
     institution = affiliation.get('institution')
     if institution:
         text = [institution] + text
-    return ", ".join(text)
+    return " ; ".join(text)
+
+def format_location(authors):
+    text = []
+    for author in authors:
+        affiliation = author['affiliation']
+        if affiliation:
+            location = affiliation.get('location')
+            if location:
+                text.extend(list(affiliation['location'].values()))
+            else:
+                text.append(' ')
+        else:
+            text.append(' ')
+        
+    return " ; ".join(text)
 
 def format_authors(authors, with_affiliation=False):
     name_ls = []
@@ -84,7 +99,7 @@ def format_authors(authors, with_affiliation=False):
         else:
             name_ls.append(name)
     
-    return ", ".join(name_ls)
+    return " ; ".join(name_ls)
 
 
 def format_body(body_text):
@@ -142,6 +157,7 @@ def generate_clean_df(all_files):
             format_authors(file['metadata']['authors']),
             format_authors(file['metadata']['authors'], 
                            with_affiliation=True),
+            format_location(file['metadata']['authors']),               
             format_body(file['abstract']),
             format_body(file['body_text']),
             format_bib(file['bib_entries']),
@@ -152,7 +168,7 @@ def generate_clean_df(all_files):
         cleaned_files.append(features)
 
     col_names = ['paper_id', 'title', 'authors',
-                 'affiliations', 'abstract', 'text', 
+                 'affiliations', 'location', 'abstract', 'text', 
                  'bibliography','raw_authors','raw_bibliography']
 
     clean_df = pd.DataFrame(cleaned_files, columns=col_names)
--- covid/data/preprocessing.py
+++ covid/data/preprocessing.py
@@ -20,15 +20,22 @@ class DataProcessor:
         self.cols_to_preproc = cols_to_preproc
         self.df_preproc = None
 
-    def read_data(self, raw_data_path):
+    def read_json_data(self, raw_data_path):
         """Read raw data into DataProcessor."""
         filenames = getListOfFiles(raw_data_path,extention='json')
         self.df = getCSVPapers(filenames,raw_data_path)
 
+    def read_csv_data(self, raw_data_path):
+        """Read raw data into DataProcessor."""
+        self.df = pd.read_csv(raw_data_path+self.filename)
+
     def process_data(self):
         """Process raw data into useful files for model."""
         self.df_preproc = nltkPreProcDf(self.df,self.cols_to_preproc)
-        self.df_preproc = datetimeCleanerPipe(self.df_preproc, 
+
+    def process_dates(self):
+        """Normalize dates"""
+        self.df_preproc = datetimeCleanerPipe(self.df, 
                                               col='publish_time', 
                                               normalize_future=True)
 
